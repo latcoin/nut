6e719171e9ebd6433d8f06106f63006134048be0
latcoin <nharan81@gmail.com>
2015-08-23 15:14:34 -0700

    blah

--- monstertruck.php
+++ monstertruck.php
@@ -21,7 +21,7 @@
 			<div id="bufferdots4"><img src="craigslist/monster/monster.jpg" alt="" width="455" height="304" /></div>
 <div id="bufferdots2"></div>
 				<div id="bufferdots3"></div>
-				<h2> <strong>$75,000</strong></h2>
+				<h2> <strong>Atlanta - $75,000</strong></h2>
 				<p>&nbsp;</p>
 				<p><strong>From:</strong> Anonymous nutter<br />
 				  <strong>To:</strong> Monster truck owner<br />
@@ -46,11 +46,37 @@
 				<br />
 				Cool, when would you like to meet up? I think this could be a very exciting venture. I've started informing people of your truck and they're lining up! <br />
 			<div id="bufferdots"></div>
-				<strong>From:</strong> Monster truck owner<br />
-                <strong>To: </strong>Anonymous nutter<br />
+				<p><strong>From:</strong> Monster truck owner<br />
+                  <strong>To: </strong>Anonymous nutter<br />
 				<br />
 				How about we meetup somewhere private? Perhaps in an alley somewhere? I'll have the keys ready for you. <br />
-				<div id="bufferdots"></div>
+		    </p>
+			<div id="bufferdots"></div>
+            <p><strong>From:</strong> Anonymous nutter<br />
+                  <strong>To: </strong>Monster truck owner<br />
+				<br />
+			Let's firm up details over email and then proceed on with a live meetup.<br />
+            </p>
+            <div id="bufferdots"></div>
+            <p><strong>From:</strong> Monster truck owner<br />
+              <strong>To: </strong>Anonymous nutter<br />
+				<br />
+            </p>
+            <p>Judging by your name u are obviously from Mexico &amp; didn't have much schooling I could care less what you think about my vehicle u see the price &amp; if u not interested don't look &amp; don't waste my time with your stupid bullshit</p>
+            <div id="bufferdots"></div>
+            <p><strong>From:</strong> Anonymous nutter<br />
+                  <strong>To: </strong>Monster truck owner<br />
+				<br />
+				Judging by your poor grammar, I'd have assumed you were the one from a foreign place. Not Mexico though, they can articulate themselves better than that of a 5 year old. If I'd have to guess, I'd say some really broke and uneducated place like Atlanta.</p>
+            <p>&nbsp;</p>
+            <p>Now listen, I'm a busy person and I don't have time to waste here. I presented you with an offer, you're either interested or you're not. Stop wasting my time please. I have important things to do.</p>
+            <div id="bufferdots"></div>
+            <p><strong>From:</strong> Monster truck owner<br />
+              <strong>To: </strong>Anonymous nutter<br />
+				<br />
+            </p>
+            <p>Lmao can you understand this   "FUCK YOU"</p>
+            <p>&nbsp;</p>
 		  </div>
           <?php
 			include('rightsidebar.php');
