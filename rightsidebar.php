b709d2b7466a6df781ec83f7d27699e9fabd7fb3
latcoin <nharan81@gmail.com>
2015-08-23 14:10:38 -0700

    test

---
+++ rightsidebar.php
@@ -0,0 +1,37 @@
+<?php 
+$page = 'rightsidebar'; 
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>It's like an involuntary tic or a really boring form of Tourette's</title>
+		<link rel="stylesheet" type="text/css" href="nuttyemails.css" />
+		<meta name="copyright" content="Anonymous Nobody">
+		<meta name="author" content="Anonymous Nobody">
+		<meta name="keywords" content="Anonymous Nobody, craigslist, pranks, prank emails, Monster truck">
+		<meta name="description" content="">
+	</head>
+
+<body>
+<div id="rightcolumn"><br />
+		    <a href="index.html"><img src="images/troll.gif" alt="" width="140" height="225" border="0" /></a><br />
+		    <h4>Contact</h4>
+		    <ul class="sidebar">
+		      <li><a href="mailto:rkari@ruggedinbox.com">Send me an email</a></li>
+		      <li><a href="mailto:rkari@ruggedinbox.com">Submit a craigslist troll post.</a></li>
+		      <li><a href="disclaimer.html">Disclaimer</a></li>
+		      <li></li>
+		      <li></li>
+		      <li></li>
+		      <li></li>
+	        </ul>
+		    <h1>News</h1>
+		    <h5>Nothing new to report<br />
+		      <br />
+		      <div id="bufferspacemenu"><font color="#888888"><a href="tiiap.html"><br />
+		        </a></font></div>
+	        </h5>
+	      </div>
+</body>
+</html>
