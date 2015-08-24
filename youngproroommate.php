9e86d264868d7458c26c4ef423193facfebbe590
latcoin <nharan81@gmail.com>
2015-08-23 23:15:40 -0700

    oops

---
+++ youngproroommate.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Nutty Emails</title>
+<link rel="stylesheet" type="text/css" href="nuttyemails.css" />
+		<meta name="keywords" content="emails, email pranks">
+		<meta name="copyright" content="Anonymous Nobody">
+		<meta name="author" content="Anonymous Nobody">
+		<meta name="description" content="This site contains none of your business. You do not have permission to access the content and if you do so you agree to waive all rights.">
+	</head>
+
+<body>
+		<div id="wrappertop"></div>
+		<div id="wrapper">
+		<?php
+            include('leftsidebar.php');
+        ?>
+
+		  <div id="rightmiddle">
+			<div id="bufferdots4"><img src="craigslist/youngproroommate/youngproroommate.png" alt="" width="455" height="508" /></div>
+<div id="bufferdots2"></div>
+				<div id="bufferdots3"></div>
+				<h2> <strong>Good ol' San Francisco - $510 for a couch!</strong></h2>
+				<p>&nbsp;</p>
+				<p><strong>From:</strong> Anonymous nutter<br />
+			    <strong>To:</strong> SF couch owner</p>
+				<p><br />
+			Hi,</p>
+			<p>&nbsp;</p>
+			<p>I'm very much interested in your couch, what make is it? I'm about 6' 5&quot; tall, would that cause any issues? Also, I have to be upfront about this, I have a mild case of tourettes.	</p>
+			<p>&nbsp;</p>
+			<p>I look forward to hearing from you.</p>
+			<p>Best,			</p>
+			<div id="bufferdots"></div>
+				<p><strong>From: </strong> SF couch owner<br />
+                <strong>To:</strong> Anonymous nutter<br />
+				  <br />
+		    Hi there!</p>
+			<p>&nbsp;</p>
+			<p>The couch make I'm unsure of but I can say first hand that you can have a great night's sleep on it. We've had several Air BnB guests extend their stay just to sleep on this couch (I'm not kidding!). As for your height, it would be a bit big for the length of the couch. How mild is it?</p>
+			<p>&nbsp;</p>
+				<p>Thanks!<br />
+		    </p>
+			<div id="bufferdots"></div>
+				<p><strong>From:</strong> Anonymous nutter<br />
+                  <strong>To: </strong> SF couch owner<br />
+				<br />
+				</p>
+			<p>Hi,</p>
+			<p>&nbsp;</p>
+			<p>It is fairly mild, I have an outburst about twice a week. Plus I have this weird habit of sleep walking, which happens every night. Unbeknownst to me I wander off into closets and other rooms and start rummaging through things looking for buried treasure. The doctor says it is due to intense trauma I suffered as a child.</p>
+			<p>&nbsp;</p>
+				<p>Best,<br />
+		    </p>
+			<div id="bufferdots"></div>
+				<p><strong>From:</strong> SF couch owner<br />
+            <strong>To: </strong>Anonymous nutter<br />
+				<br />
+				I'm sorry to hear that! Due to your medical condition, I'm afraid I don't think we can rent the couch to you at this time. Best of luck in your search!<br />
+	        </p>
+			<div id="bufferdots"></div>
+				<p><strong>From:</strong> Anonymous nutter<br />
+            <strong>To: </strong>SF couch owner<br />
+				<br />
+			But I really like the couch! After you said all those things about it, I feel like I simply must experience sleeping on it. I'm willing to pay you extra for the hassle, I can throw in an extra $100. Please let me know.<br />
+		    </p>
+			<div id="bufferdots"></div>
+            <p><strong>From:</strong> SF couch owner<br />
+            <strong>To: </strong>Anonymous nutter<br />
+			  <br />
+			  Okay discussed it with the roommate and if you're willing to pay $200 extra + pay for adding locks to our bedrooms, we will be able to acommodate you. Is there anything else you're suffering from?? lol.<br />
+		    </p>
+			<div id="bufferdots"></div>
+            <p><strong>From:</strong> Anonymous nutter<br />
+            <strong>To: </strong>SF couch owner<br />
+			  <br />
+		    Hi,</p>
+            <p>&nbsp;</p>
+            <p>That's good to know! No, I'm not suffering from anything else... except.. I have this rash on my *well you know* penis. The doctor says it isn't contageous as long as my skin doesn't come in contact with things. Don't worry though, half of the time I'll be fully dressed when I'm on your couch.<br />
+            </p>
+			<div id="bufferdots"></div>
+            <p><strong>From:</strong> SF couch owner<br />
+            <strong>To: </strong>Anonymous nutter</p>
+            <p>&nbsp;</p>
+            <p>hmm. craigslist has a strong policy on harassment of its members. dont message me again shithead!</p>
+            <p>&nbsp;</p>
+            <p>Sent from my iPhone<br />
+            </p>
+			<div id="bufferdots"></div>
+            <p><strong>From:</strong> Anonymous nutter<br />
+            <strong>To: </strong>SF couch owner</p>
+            <p>&nbsp;</p>
+            <p>Whoa! what is with the hostility here? I thought we were getting along, what happened? Btw, I've just read the bylaws of the California tenancy agreement and you can't discriminate tenants based on medical conditions. So let's firm up dates on when I can come move in. I agree to your rent and am looking forward to sleeping on that amazing couch.<br />
+            </p>
+			<div id="bufferdots"></div>
+            <p><strong>From:</strong> SF couch owner<br />
+            <strong>To: </strong>Anonymous nutter</p>
+            <p>&nbsp;</p>
+            <p>Do you even have a job? Do you work? Or are you still living in mommy's basement jerking off to kiddie porn? </p>
+            <p>&nbsp;</p>
+            <p>All of the above were rhetorical jerk off, so don't respond.</p>
+            <p><br />
+            </p>
+			<div id="bufferdots"></div>
+            <p><strong>From:</strong> Anonymous nutter<br />
+            <strong>To: </strong>SF couch owner</p>
+            <p>&nbsp;</p>
+            <p><img src="craigslist/youngproroommate/job.png" alt="" width="455" height="403" /><br />
+            </p>
+			<div id="bufferdots"></div>
+		  </div>
+          <?php
+			include('rightsidebar.php');
+		  ?>
+		  
+		</div>
+		
+		<div id="wrapperend">
+		  <div id="leftcolumn2"></div>
+		</div>
+	</body>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-66650897-1', 'auto');
+  ga('send', 'pageview');
+
+	</script>
+</html>
