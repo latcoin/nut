b906dcb7fe20cc1e71c4eb1c949ca812d5cbd9e8
latcoin <nharan81@gmail.com>
2015-08-28 21:30:11 -0700

    fb like

--- nolisterine.php
+++ nolisterine.php
@@ -3,6 +3,14 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Nutty Emails</title>
+    <!-- You can use open graph tags to customize link previews.
+    Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
+    <meta property="og:url"           content="http://nuttyemails.com/nolisterine.php" />
+    <meta property="og:type"          content="website" />
+    <meta property="og:title"         content="Craigslist racist meets troll" />
+    <meta property="og:description"   content="Craigslist racist gets trolled hard" />
+    <meta property="og:image"         content="http://nuttyemails.com/craigslist/nofirstnations/nofirstnations.png" />
+    
 <link rel="stylesheet" type="text/css" href="nuttyemails.css" />
 		<meta name="keywords" content="emails, email pranks">
 		<meta name="copyright" content="Anonymous Nobody">
@@ -11,6 +19,18 @@
 	</head>
 
 <body>
+<!-- This section is code from facebook for like and share initialization -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=919855458072430";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+
+
 		<div id="wrappertop"></div>
 		<div id="wrapper">
 		<?php
@@ -72,6 +92,15 @@
             <p>&nbsp;</p>
             <p>Fuck off! don't message me again</p>
             <p>&nbsp;</p>
+            
+            <!-- Your like button code -->
+            <div class="fb-like" 
+                data-href="http://nuttyemails.com/nolisterine.php" 
+                data-layout="standard" 
+                data-action="like" 
+                data-show-faces="true">
+            </div>
+            
 		  </div>
           <?php
 			include('rightsidebar.php');
