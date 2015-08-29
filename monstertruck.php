4e8fc3423dc94e11731d13fff7038c98547c741d
latcoin <nharan81@gmail.com>
2015-08-29 00:00:22 -0700

    dds

--- monstertruck.php
+++ monstertruck.php
@@ -3,6 +3,13 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Monster truck owner gets a weird request from a troll</title>
+	<!-- You can use open graph tags to customize link previews.
+    Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
+    <meta property="og:url"           content="http://nuttyemails.com/monstertruck.php" />
+    <meta property="og:type"          content="website" />
+    <meta property="og:title"         content="Monster truck owner gets a weird request from a troll" />
+    <meta property="og:description"   content=" " />
+    <meta property="og:image"         content="http://nuttyemails.com/craigslist/louisvuittonisnotapurse/louisvuitton.jpg" />
     
 <link rel="stylesheet" type="text/css" href="nuttyemails.css" />
 		<meta name="keywords" content="emails, email pranks">
@@ -96,7 +103,7 @@
                 data-action="like" 
                 data-show-faces="true"
                 data-share="true">
-            </div>  
+            </div>
 		  </div>
           <?php
 			include('rightsidebar.php');
